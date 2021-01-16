0dc0e45b5154e0659f77cad1f7d402e45854250e
jaikishorgohil <jai.k.gohil@gmail.com>
2021-01-16 22:43:41 +0530

    Initial commit and adding Web Socket Configuration

---
+++ src/test/java/com/jkproduction/GroupChat/GroupChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jkproduction.GroupChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
